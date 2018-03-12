f2adcc04c84f1e009574417df41dfaa315db08c2
boedy <boudewijng@gmail.com>
2018-03-12 22:17:12 +0100

    Remove lock on signal exit

--- src/Process.js
+++ src/Process.js
@@ -1,4 +1,9 @@
 const lockfile = require('lockfile');
+var signalExit = require('signal-exit')
+signalExit(function (code, signal) {
+  if (signal)
+    process.emit('exit')
+})
 
 module.exports.lock = () => {
   return new Promise(function(resolve, reject){
